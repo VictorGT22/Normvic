cdca622a7f03ecb89c7856b12704b951aaf1c8ed
VictorGT22 <victor22555@gmail.com>
2023-07-04 12:28:24 +0200

    [UPGRADE] check indentations

--- zzzzprueba/preuba_carpeta/dentro_carpeta.c
+++ zzzzprueba/preuba_carpeta/dentro_carpeta.c
@@ -6,7 +6,7 @@
 /*   By: vics <vics@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 18:14:22 by vics              #+#    #+#             */
-/*   Updated: 2023/07/04 12:26:12 by vics             ###   ########.fr       */
+/*   Updated: 2023/07/04 12:28:05 by vics             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ void	print_linked(lst_dir *lst)
 	lst_dir	*temp;
 
 	temp = lst;
-	while (temp)
+	while (temp
+		&& prueba > 3)
 	{
 		printf("prueba\n");
 	}
