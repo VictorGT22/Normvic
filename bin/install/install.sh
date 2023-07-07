0bc69894aec4136bfc0b9718de2191b100e2bd0e
VictorGT22 <victor22555@gmail.com>
2023-07-07 21:00:23 +0200

    [ADD] install.sh

--- bin/install/install.sh
+++ bin/install/install.sh
@@ -12,15 +12,10 @@ alias_name="normvic"
 alias_command="ls -l"
 
 Verifica si el archivo .bashrc existe y no contiene ya el alias
-if [ -f "$HOME/.bashrc" ] && ! grep -qF "alias $alias_name="; then
+if [ -f "$HOME/.bashrc" ]; then
     echo "alias $alias_name=\"$alias_command\"" >> "$HOME/.bashrc"
     echo "Alias agregado al archivo .bashrc"
 fi
-
-Verifica si el archivo .zshrc existe y no contiene ya el alias
-if [ -f "$HOME/.zshrc" ] && ! grep -qF "alias $alias_name=" "$HOME/.zshrc"; then
-    echo "alias $alias_name=\"$alias_command\"" >> "$HOME/.zshrc"
-    echo "Alias agregado al archivo .zshrc"
-# 
+ 
 exec "$SHELL"
 
