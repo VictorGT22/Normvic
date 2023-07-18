138abcadf84bde21db8bff21596f6442cd87977d
VictorGT22 <victor22555@gmail.com>
2023-07-18 14:11:33 +0200

    [IMPROVE] basic operators

--- zzzzprueba/preuba_carpeta/dentro_carpeta.c
+++ zzzzprueba/preuba_carpeta/dentro_carpeta.c
@@ -6,7 +6,7 @@
 /*   By: vics <vics@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 11:16:44 by vics              #+#    #+#             */
-/*   Updated: 2023/07/18 13:28:27 by vics             ###   ########.fr       */
+/*   Updated: 2023/07/18 14:04:03 by vics             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,10 @@ void	correct_header_2(s_variables *var, lst_dir *lst, int *i)
 	struct tm	*info_tiempo;
 	char		buffer[80];
 
-	if (*prueba && prueba_2)
+	if (*prueba && &prueba_2)
 		prueba = *i + *i;
+	j = (index == -1) ? ft_strlen(lst->info[*i]) - 3 : index - 2;
+	index = ft_strchr_nocomented(lst->info[*i], '(');
+	if (index != -1 && index < j)
+		j = ft_strchr_nocomented(lst->info[*i], '(') + 1;
 }
